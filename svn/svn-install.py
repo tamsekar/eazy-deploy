955b63c9bc2ab3e2ec9cf50a27f4d83884263714
atamsekar <anirudh.tamsekar@gmail.com>
2017-08-17 17:08:40 +0530

    Second Commit
    Added syspack.sh file to identify system packages. Modified svn-install.py to call syspack.sh
    
    [ticket: 1]

--- svn/svn-install.py
+++ svn/svn-install.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from sys import platform
+import subprocess
+from subprocess import Popen, PIPE
 
 def platform_check():
     if platform == "linux" or platform == "linux2":
@@ -12,11 +14,16 @@ def platform_check():
     else:
         print "Unknown OS"
 
+def call_shell():
+    session = subprocess.Popen(['test.sh'], stdout=PIPE, stderr=PIPE)
+    stdout, stderr = session.communicate()
 
-print "platform.linux_distribution()"
+    if stderr:
+        raise Exception("Error "+str(stderr))
 
 def main():
     platform_check()
+    call_shell()
 
 if __name__ == "__main__":
     main()
