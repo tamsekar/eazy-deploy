eaedce8ed2b9968376e1251416f724a894ffe8f6
atamsekar <anirudh.tamsekar@gmail.com>
2017-08-18 15:03:28 +0530

    Added a new file as second version

---
+++ svn/svn_setcon.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python
+
+
+import sys
+import platform
+import os
+from __future__ import print_function
+from collections import OrderedDict
+import pprint
+
+
+# Color Formattijng
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+print bcolors.HEADER + "OS Information"
+      + bcolors.ENDC
+
+
+print_function
+
+def dist():
+    print("""
+    System: %s
+    Node Name: %s
+    Release: %s
+    Version: %s
+    Machine: %s
+    Architecture: %s
+    Distribution: %s
+    """ % (
+    platform.uname()[0],
+    platform.uname()[1],
+    platform.release(),
+    platform.uname()[3],
+    platform.uname()[4],
+    platform.architecture()[0],
+    platform.linux_distribution()[0],
+    ))
+
+
+def main():
+    platform_check()
+    call_shell()
+    linux_distribution()
+
+if __name__ == "__main__":
+    main()
