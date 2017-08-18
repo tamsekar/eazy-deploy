4aa9e5e69d455499a56fb372d28aca0df323fb13
atamsekar <anirudh.tamsekar@gmail.com>
2017-08-18 17:08:14 +0530

    Added new functions

--- svn/svn_setcon.py
+++ svn/svn_setcon.py
@@ -2,12 +2,16 @@
 
 from __future__ import print_function
 from collections import OrderedDict
+from collections import namedtuple
+import pwd
+import glob
+import re
 import sys
 import platform
 import os
 import pprint
 import pdb
-pdb.set_trace()
+# pdb.set_trace()
 
 
 # Color Formatting
@@ -75,18 +79,84 @@ def cpuinfo():
 
     return cpuinfo
 
+def meminfo():
+    ''' Return the information in /proc/meminfo
+    as a dictionary '''
+    meminfo=OrderedDict()
 
+    with open('/proc/meminfo') as f:
+        for line in f:
+            meminfo[line.split(':')[0]] = line.split(':')[1].strip()
+    return meminfo
+
+
+def netdevs():
+    ''' RX and TX bytes for each of the network devices '''
+
+    with open('/proc/net/dev') as f:
+        net_dump = f.readlines()
+
+    device_data={}
+    data = namedtuple('data',['rx','tx'])
+    for line in net_dump[2:]:
+        line = line.split(':')
+        if line[0].strip() != 'lo':
+            device_data[line[0].strip()] = data(float(line[1].split()[0])/(1024.0*1024.0),
+                                                float(line[1].split()[8])/(1024.0*1024.0))
+
+    return device_data
+
+def process_list():
+
+    pids = []
+    for subdir in os.listdir('/proc'):
+        if subdir.isdigit():
+            pids.append(subdir)
+
+    return pids
+
+dev_pattern = ['sd.*','mmcblk*']
+
+def size(device):
+    nr_sectors = open(device+'/size').read().rstrip('\n')
+    sect_size = open(device+'/queue/hw_sector_size').read().rstrip('\n')
+
+    # The sect_size is in bytes, so we convert it to GiB and then send it back
+    return (float(nr_sectors)*float(sect_size))/(1024.0*1024.0*1024.0)
+
+def detect_devs():
+    for device in glob.glob('/sys/block/*'):
+        for pattern in dev_pattern:
+            if re.compile(pattern).match(os.path.basename(device)):
+                print('Device:: {0}, Size:: {1} GiB'.format(device, size(device)))
+
+
+def getusers():
+    users = pwd.getpwall()
+    for user in users:
+        print('{0}:{1}'.format(user.pw_name, user.pw_shell))
 
 def sysstat():
     cpuinfo()
 
 def main():
     dist()
-
+    detect_devs()
+    getusers()
 
 if __name__ == "__main__":
     main()
     cpuinfo()
     cpuinfo = cpuinfo()
     for processor in cpuinfo.keys():
-        print (cpuinfo[processor]['model name'])
+        print ("""
+        CPU: %s
+        """ % (cpuinfo[processor]['model name']))
+    meminfo = meminfo()
+    print('Total memory: {0}'.format(meminfo['MemTotal']))
+    print('Free memory: {0}'.format(meminfo['MemFree']))
+    netdevs = netdevs()
+    for dev in netdevs.keys():
+        print('{0}: {1} MiB {2} MiB'.format(dev, netdevs[dev].rx, netdevs[dev].tx))
+    pids = process_list()
+    print('Total number of running processes:: {0}'.format(len(pids)))
